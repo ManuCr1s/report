9d3d186dfc201d5af8acc069b89dbc7a0c4c860b
mcristobaln <mcristobal@outlook.com>
2023-03-09 17:54:30 -0500

    feat: Add migration reports table and default migrations of laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->dateTime('created_at', $precision = 3);
+            $table->dateTime('updated_at', $precision = 3);
         });
     }
 
--- database/migrations/2023_03_09_221528_create_reports_table.php
+++ database/migrations/2023_03_09_221528_create_reports_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->dateTime('last_used_at', $precision = 3)->nullable();
+        Schema::create('reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code');
+            $table->string('asunto');
+            $table->dateTime('date_at',$precision=3);
             $table->dateTime('created_at', $precision = 3);
             $table->dateTime('updated_at', $precision = 3);
         });
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('reports');
     }
 };
